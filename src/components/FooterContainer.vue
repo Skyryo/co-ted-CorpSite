d89c0ce41924273e623f6d69c13a7e028cb5b868
skyryo <ryosakaiwmagptja@gmail.com>
2022-09-17 03:40:42 +0900

    サイトフレーム

---
+++ src/components/FooterContainer.vue
@@ -0,0 +1,23 @@
+<template>
+    <footer>
+        <div class="footer-menu">
+           <li class="footer-menu-item"><a href="#">ホーム</a></li> 
+           <li class="footer-menu-item"><a href="#">お問い合わせ</a></li> 
+           <li class="footer-menu-item"><a href="#">プライバシーポリシー</a></li> 
+        </div>
+        <small class="copy-right">2022 Co-ted Inc.</small>
+    </footer>
+</template>
+<style scoped>
+    .footer-menu {
+        background-color: #2D0425;
+        padding: 40px 0;
+        margin: 0 auto;    
+    }
+    .footer-menu-item {
+        text-align: center;
+    }
+    .copy-right {
+        text-align: center;
+    }
+</style>
---
+++ src/components/HeaderContainer.vue
@@ -0,0 +1,17 @@
+<template>
+    <section class="header-container">
+        <h1 class="header-logo">
+            <img src="../assets/img/headerLogo.png" alt="OASIS logo">
+        </h1>
+        <nav class="header-menu navbar navbar-expand-md">
+            <ul class="navbar-nav">
+                <li class="nav-item"><a class="nav-link" href="#section-01">会社概要</a></li>
+                <li class="nav-item"><a class="nav-link" href="#section-02">代表メッセージ</a></li>
+                <li class="nav-item"><a class="nav-link" href="#section-03">事業内容</a></li>
+                <li class="nav-item"><a class="nav-link" href="#section-04">お問い合わせ</a></li>
+            </ul>
+        </nav>
+    </section>
+</template>
+<style scoped>
+</style>
